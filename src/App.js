36f7bc1a90c5019f122b93105495fc8e2a79272a
kmlxx <lkm89052@gmail.com>
2025-06-18 13:51:26 +0900

    JSX 기초 실습: 변수 출력 및 기본 구조 작성

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const name = '이강민';
+    const today = new Date().toLocaleDateString();
+    const desc = 'React를 시작하는 개발자';
   return (
     <div>
-      <h1>안녕하세요, 첫 번째 React 앱 입니다!</h1>
+      <h1>{name}의 React</h1>
+        <p>오늘 날짜 : {today}</p>
+        <p>{desc}</p>
     </div>
   );
 }
